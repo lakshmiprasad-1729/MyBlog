b8dcf568fac2c47ddebc7b326b98129814124979
lakshmiprasad_1729 <panchagirilakshminarayana@gamil.com>
2024-11-03 15:55:22 +0530

    small update

--- src/main.jsx
+++ src/main.jsx
@@ -19,8 +19,6 @@ import './index.css'
 import Login from './components/BasicComponents/Login.jsx';
 import App from './App.jsx'
 import TinyEditor from './components/Editor/Editor.jsx';
-import { useEffect } from 'react';
-
 
 
 const router = createBrowserRouter([
